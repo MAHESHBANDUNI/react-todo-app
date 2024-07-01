fd88b9d7c37e95d2306ca0e41528c3a174e344d3
Mahesh Banduni <114943739+MAHESHBANDUNI@users.noreply.github.com>
2024-07-01 19:22:37 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from './reducers';
 
+/**
+ * Creates and configures the Redux store
+ * Enables Redux DevTools if available in the browser
+ */
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
