9a2d7ca127b4d508b035571f063b4ce1b6ae2ec4
Mahesh Banduni <114943739+MAHESHBANDUNI@users.noreply.github.com>
2024-07-01 19:18:43 +0530

    Update TaskList.js

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import TaskItem from './TaskItem';
 
+/**
+ * TaskList Component
+ * 
+ * This component renders a list of TaskItem components.
+ * It uses useSelector to get the tasks from the Redux store.
+ */
 const TaskList = () => {
+  // Select tasks from the Redux store
   const tasks = useSelector(state => state.tasks);
 
   return (
